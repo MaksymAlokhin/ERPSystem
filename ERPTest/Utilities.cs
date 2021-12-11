982bbf8e215089c880e0c8893223b0ef76047b45
gform <maksym.alokhin@protonmail.com>
2021-12-11 11:53:51 +0200

    Added Test Project

---
+++ ERPTest/Utilities.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ERPSystem.Data;
+
+namespace ERPTest.Tests
+{
+    public static class Utilities
+    {
+        #region snippet1
+        public static DbContextOptions<ApplicationDbContext> TestDbContextOptions()
+        {
+            // Create a new service provider to create a new in-memory database.
+            var serviceProvider = new ServiceCollection()
+                .AddEntityFrameworkInMemoryDatabase()
+                .BuildServiceProvider();
+
+            // Create a new options instance using an in-memory database and 
+            // IServiceProvider that the context should resolve all of its 
+            // services from.
+            var builder = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase("InMemoryDb")
+                .UseInternalServiceProvider(serviceProvider);
+
+            return builder.Options;
+        }
+        #endregion
+    }
+}
